594ca26371f6e49c7b1d68952103ea81b0705a80
Olga <oluaribalka@yandex.ru>
2024-01-26 23:19:12 +0300

    use .env and stdin

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const theTypesCounterInTheObject = require('the-types-counter-in-the-object-node-js');
 
-const result = theTypesCounterInTheObject.createObjectOfTypes({ cat: {name: 'Laska', age: '3'}, book: ['Луна суровая хозяйка', 'Стоик'], field: null });
+// через переменные окружения
+require('dotenv').config();
+const data = process.env.OBJECT;
+const dataString = String(data);
+const dataObject = JSON.parse(dataString);
+const result = theTypesCounterInTheObject.createObjectOfTypes(dataObject);
 
 console.log(result);
+
+// через стандартный ввод
+process.stdin.resume();
+process.stdin.on('data', (data) => { // вводить в формате JSON
+    const dataString = String(data); 
+    const dataObject = JSON.parse(dataString);
+    const result = theTypesCounterInTheObject.createObjectOfTypes(dataObject);
+
+    console.log(result);
+});
